ce9ac1f08f3451b466c2b94d98b84783d208f10c
wchang88 <60525612+wchang88@users.noreply.github.com>
2023-04-20 22:20:23 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='TaiwanMandarinG2P',
+    version='0.0.1',
+    install_requires=[
+        'gw2pw',
+        'pyzhuyin',
+        'pypinyin'
+    ],
+)
